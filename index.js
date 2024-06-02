6a6ad9f42f3a9e00aa12a135388c02dd7460d98c
ayjvv <ayjvv@naver.com>
2024-06-03 02:39:16 +0900

    윤지 포폴]

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+
+/*** a태그 속성지움 ***/
 $('a[href="#"]').on('click', e => e.preventDefault());
+
 
 /*** 메뉴 열고 닫음 ***/
 $(document).ready(function() {
@@ -23,42 +26,108 @@ $(document).ready(function() {
     });
 });
 
+//   $(document).ready(function() {
+//     $('.circle').on({
+//         'mouseenter': function() {
+//             $(this).addClass('none');
+//         },
+//         'mouseleave': function() {
+//             $(this).find('li span').removeClass('hovered');
+//         }
+//     });
+// });
 
 
 
-$(document).ready(function() {
-  $('.circle').hover(
-    function() {
-      $('body').stop().animate({
-        'background-color': 'linear-gradient(#FEC1C3,#FDCFC5)'
-      }, 1500); 
-    },
-    function() {
-      $('body').stop().animate({
-        'background-color': '#F0EBE8'
-      }, 1200); 
-    }
-  );
-});
+/*** 메인 배경컬러 바꿈 ***/
+// $(document).ready(function() {
+//   $('.circle').hover(
+//     function() {
+//       $('.cover, .project p').stop().animate({
+//         'background-color': 'linear-gradient(#FEC1C3,#FDCFC5)'
+//       }, 1500); 
+//     },
+//     function() {
+//       $('.cover, .project p').stop().animate({
+//         'background-color': '#F0EBE8'
+//       }, 1200); 
+//     }
+//   );
+// });
+
+
+// $(document).ready(function() {
+//   $('.circle').hover(
+//       function() {
+//           $('.cover').stop().css({
+//               'background': 'linear-gradient(#FEC1C3,#FDCFC5)',
+//           });
+//       },
+//       function() {
+//           $('.cover').stop().css({
+//               'background': 'linear-gradient(#F0EBE8,#F0EBE8)'
+//           });
+//       }
+//   );
+// });
 
 
 
- /******* header 사라짐 **********/
+
+ /*** 메뉴 사라짐 ***/
  let lastScrollTop = 0;
 
  $(window).on('scroll', () => {
-   // 현재 스크롤 위치를 가져옴
    let currentScrollTop = $(window).scrollTop();
    
    // 현재 스크롤 위치와 이전 스크롤 위치를 비교
    if (currentScrollTop > lastScrollTop) {
-     // 스크롤을 내릴 때
-     $('header').addClass('none on');
+     $('.menu-btn').addClass('none on');
    } else {
-     // 스크롤을 올릴 때
-     $('header').removeClass('none on');
+     $('.menu-btn').removeClass('none on');
    }
    
    // 이전 스크롤 위치를 현재 스크롤 위치로 업데이트
    lastScrollTop = currentScrollTop;
  });
+
+
+
+
+
+ /*** 스크롤에 따라 스와이퍼 배경색 바꾸기 ***/
+$(document).ready(function() {
+  $(window).on('scroll', function() {
+    var scrollPosition = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    var swiperTop = $('.project-txt').offset().top;
+    var swiperBottom = swiperTop + $('.project-txt').outerHeight();
+
+    if (scrollPosition + windowHeight > swiperTop && scrollPosition < swiperBottom) {
+      $('.project-slide1').stop().animate({
+        'background-color': 'linear-gradient(#E3D3FF)'
+      }, 700); //안에 들어왔을 때
+    } else {
+      
+      $('.project-slide1').stop().animate({
+        'background-color': '#F0EBE8'
+      }, 300); // 벗어났을 때
+    }
+  });
+});
+
+
+//  $(document).ready(function() {
+//   $('.mySwiper').hover(
+//     function() {
+//       $('.project-slide1').stop().animate({
+//         'background-color': 'linear-gradient(#E8DBFF)'
+//       }, 1100); 
+//     },
+//     function() {
+//       $('.project-slide1').stop().animate({
+//         'background-color': '#F0EBE8'
+//       }, 1100); 
+//     }
+//   );
+// });
