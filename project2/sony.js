1fc44957f9ad11199a10a7cdba75543d9887da14
ajyvv <ayjvv@naver.com>
2024-06-26 17:38:59 +0900

    윤지

--- project2/sony.js
+++ project2/sony.js
@@ -21,4 +21,47 @@ window.addEventListener('load', function() {
 });
 
 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4.2,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination1",
+    clickable: true,
+  },
+});
 
+
+
+var swiper = new Swiper(".mySwiper1", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 2.5,
+  initialSlide: 1, // 두 번째 슬라이드에서 시작
+  coverflowEffect: {
+    rotate: 50,
+    stretch: -50,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination2",
+  },
+});
+
+
+var swiper = new Swiper(".mySwiper3", {
+  slidesPerView: 2, // 자동 슬라이드 크기 조정
+  centeredSlides: false, // 가운데 정렬 비활성화
+  spaceBetween: -550,
+  initialSlide: 0, // 첫 번째 슬라이드부터 시작
+  pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+  },
+  navigation: {
+      nextEl: ".swiper-button-next1",
+      prevEl: ".swiper-button-prev1",
+  },
+});
